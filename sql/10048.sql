bdc6055a93bb556a1c951d004f031aa608368fd7
adibandla <adityabandla@gmail.com>
2025-09-25 21:36:16 -0400

    Update 10048.sql

--- sql/10048.sql
+++ sql/10048.sql
@@ -2,7 +2,6 @@
 https://platform.stratascratch.com/coding/10048-top-businesses-with-most-reviews?code_type=1
 Find the top 5 businesses with most reviews. Assume that each row has a unique business_id such that the total reviews for each business is listed on each row. Output the business name along with the total number of reviews and order your results by the total reviews in descending order.
 
-
 If there are ties in review counts, businesses with the same number of reviews receive the same rank, and subsequent ranks are skipped accordingly (e.g., if two businesses tie for rank 4, the next business receives rank 6, skipping rank 5).
 
 Difficulty: Medium
@@ -35,4 +34,4 @@ FROM(
     FROM yelp_business
 )
 WHERE score_rank <= 5
-ORDER BY month
+ORDER BY month;
