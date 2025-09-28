f067f25c09b82853f7e497cdc57c524b36c3b2ef
adibandla <adityabandla@gmail.com>
2025-09-28 12:39:46 -0400

    Create 10350.sql

---
+++ sql/10350.sql
@@ -0,0 +1,42 @@
+/*
+https://platform.stratascratch.com/coding/10350-algorithm-performance?code_type=1
+Meta/Facebook is developing a search algorithm that will allow users to search through their post history. You have been assigned to evaluate the performance of this algorithm.
+We have a table with the user's search term, search result positions, and whether or not the user clicked on the search result.
+Write a query that assigns ratings to the searches in the following way:
+
+•	If the search was not clicked for any term, assign the search with rating=1
+•	If the search was clicked but the top position of clicked terms was outside the top 3 positions, assign the search a rating=2
+•	If the search was clicked and the top position of a clicked term was in the top 3 positions, assign the search a rating=3
+
+As a search ID can contain more than one search term, select the highest rating for that search ID. Output the search ID and its highest rating.
+Example: The search_id 1 was clicked (clicked = 1) and its position is outside of the top 3 positions (search_results_position = 5), therefore its rating is 2.
+
+Difficulty: Hard
+
+Tables:
+<yelp_business>
+clicked						bigint
+search_id					bigint
+search_results_position		bigint
+search_term					text
+*/
+
+WITH
+  unclicked_ratings AS (
+    SELECT
+      search_id,
+      CASE WHEN SUM(clicked) = 0 THEN 1 ELSE 0 END AS rating
+    FROM fb_search_events
+    GROUP BY search_id
+  ),
+  clicked_ratings AS (
+    SELECT
+      search_id,
+      CASE WHEN MIN(search_results_position) > 3 THEN 2 ELSE 3 END AS rating
+    FROM fb_search_events
+    WHERE clicked = 1
+    GROUP BY search_id
+  )
+SELECT * FROM unclicked_ratings WHERE rating = 1
+UNION ALL
+SELECT * FROM clicked_ratings;
